a12d3b9394d64ce8d460a677c6693a69852e2bbb
Gerson <agersonjuarez123@gmail.com>
2020-11-11 11:09:45 -0600

    make it better

--- index.js
+++ index.js
@@ -296,7 +296,7 @@ io.on('connection',connected);
                                                     console.log('cantidad de jugadores: '+cantjug);
 
 
-                                                    socket.broadcast.emit ( 'cantidad' , {cantidad:cantjug,sala:roomm.nombremesa,cod:salroom});
+                                                    socket.broadcast.emit ( 'aumentarcantidad' , {cantidad:cantjug,sala:roomm.nombremesa,cod:salroom});
 
     
                                                 
@@ -519,7 +519,7 @@ io.on('connection',connected);
                                                                  if (index > -1) {
              
                                                                      Salas.splice(index, 1);
-                                                                     socket.broadcast.emit ( 'cantidad' , {cantidad:0,sala:'no hay',cod:sala});
+                                                                     socket.broadcast.emit ( 'eliminasala' , {cantidad:0,sala:'no hay',cod:sala});
              
                                                                   }
              
@@ -528,7 +528,7 @@ io.on('connection',connected);
                                                                  let message=`${socket.username} abandonó la partida`
                                                                  socket.to(sala).emit('abandonar', message)
                                                                  
-                                                                 socket.broadcast.emit ( 'cantidad' , {cantidad:cantjug,sala:roomm.nombremesa,cod:sala});
+                                                                 socket.broadcast.emit ( 'aumentarcantidad' , {cantidad:cantjug,sala:roomm.nombremesa,cod:sala});
              
                                                              }
                                                          }
@@ -570,14 +570,14 @@ io.on('connection',connected);
                                                     if (index > -1) {
 
                                                         Salas.splice(index, 1);
-                                                        socket.broadcast.emit ( 'cantidad' , {cantidad:0,sala:'no hay',cod:sala});
+                                                        socket.broadcast.emit ( 'eliminasala' , {cantidad:0,sala:'no hay',cod:sala});
 
                                                      }
 
                                                 }else{
                                                     const cantjug=io.nsps['/'].adapter.rooms[sala].length;
 
-                                                    socket.broadcast.emit ( 'cantidad' , {cantidad:cantjug,sala:roomm.nombremesa,cod:sala});
+                                                    socket.broadcast.emit ( 'aumentarcantidad' , {cantidad:cantjug,sala:roomm.nombremesa,cod:sala});
 
                                                 }
                                             }
