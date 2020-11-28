1c027a9fa37129aafc0dd53512823f2be8560dd6
Gerson <agersonjuarez123@gmail.com>
2020-11-27 18:16:02 -0600

    make it better

--- index.js
+++ index.js
@@ -237,7 +237,7 @@ io.on('connection',connected);
                                                                   room.remainingSeconds = room.tiempo % 60; //calcula los segundos
                                                                 
                                                                 if (typeof room.tiempo !== 'undefined') {
-                                                                    if (room.remainingSeconds== 0) {
+                                                                    if (room.tiempo <= 0) {
 
                                                                         cartas(temp);
                                                                         clearInterval(intervalo);
@@ -325,7 +325,7 @@ io.on('connection',connected);
                                                         room.remainingSeconds = room.tiempo % 60; //calcula los segundos
                                                                     
                                                                     if (typeof room.tiempo !== 'undefined') {
-                                                                        if (room.remainingSeconds == 0) {
+                                                                        if (room.tiempo<= 0) {
     
                                                                             cartas(salroom);
                                                                             clearInterval(intervalo);
